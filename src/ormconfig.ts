cf104c6e7d66d6c35af885b210a6f9a545d44654
akshatsh0610 <akshatsharma0610@gmail.com>
2025-06-30 10:03:20 +0530

    Modifications

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,15 +1,16 @@
 // src/ormconfig.ts
 import { DataSource } from "typeorm";
+import * as dotenv from 'dotenv';
 import { Book } from "./entities/Book";
 import { Review } from "./entities/Review";
-
+dotenv.config();
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "127.0.0.1",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "book_review",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: false,
   logging: true,
   entities: [Book, Review],
